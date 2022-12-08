69c5db0cd51fd05ce471f543562555647206ca9e
vovitolog <vovitolog@gmail.com>
2022-12-08 22:16:03 +0300

    unit2: objects

---
+++ src/02-objects/02.ts
@@ -0,0 +1,51 @@
+type StydentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    adress: AdressType
+    technologies: TechnologyType[]
+}
+
+type LocalCityType = {
+    title: string
+    country: string
+}
+
+type AdressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechnologyType = {
+    id: number,
+    title: string
+}
+
+const student: StydentType = {
+    id: 1,
+    name: 'Vladimir',
+    age: 37,
+    isActive: true,
+    adress: {
+        streetTitle: 'Ivanovskaya',
+        city: {
+            title: 'Khimki',
+            country: 'Russia'
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'HTML',
+        },
+        {
+            id: 2,
+            title: 'JS',
+        },
+        {
+            id: 3,
+            title: 'React',
+        },
+    ]
+}
